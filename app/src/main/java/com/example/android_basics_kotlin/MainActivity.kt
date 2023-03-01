2277961eb7769d2164a9eb3261d12d70ceb5fe82
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2023-03-01 11:05:58 +0530

    Create reference to textView and button

--- app/src/main/java/com/example/android_basics_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/android_basics_kotlin/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.android_basics_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        val resultTextView: TextView = findViewById(R.id.textView)
+        rollButton.setOnClickListener{
+            resultTextView.text = "6"
+            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
+        }
     }
 }
